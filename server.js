c9f48e842f61717ef425c981178dd14522f741f4
seanfitz34 <seanwilliamfitzgerald@gmail.com>
2022-10-12 18:09:01 -0700

    updating

--- server.js
+++ server.js
@@ -1,47 +1,47 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const path = require('path');
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
 
 // helper function
-const helpers = require('./utils/helpers');
+const helpers = require("./utils/helpers");
 
 // handlebars
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({helpers});
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
 
 // session (connects session to sequelize Database)
-const session = require('express-session');
+const session = require("express-session");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-// create session 
+// create session
 const sess = {
   secret: "super super secret",
   cookie: { originalMaxAge: 600000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // set Handlebars as the default template engine
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
